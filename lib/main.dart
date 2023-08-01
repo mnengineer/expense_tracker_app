6cd0289ab519d0ad73fdd97004b9fcbb960f0e0e
mnengineer <masashiii0115@gmail.com>
2023-08-01 16:46:23 +0700

    feat: colorSchemeの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(155, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
         useMaterial3: true,
-        scaffoldBackgroundColor: Colors.purple,
+        colorScheme: kColorScheme,
       ),
       home: const Expenses(),
     ),
